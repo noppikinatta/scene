e0a7b1d9876ae695c2204c69d516add65dbf4175
noppikinatta <noppikitouroku@gmail.com>
2024-09-06 22:27:37 +0900

    add comment

--- img.go
+++ img.go
@@ -10,6 +10,7 @@ import (
 var (
 	dummyImageBase = ebiten.NewImage(3, 3)
 
+	// DummyWhitePixel is a 1x1 white pixel image.
 	DummyWhitePixel = dummyImageBase.SubImage(image.Rect(1, 1, 2, 2)).(*ebiten.Image)
 )
 
