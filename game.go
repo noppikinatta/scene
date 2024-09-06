572966ba9e133d46094d32722422116ac19c348c
noppikinatta <noppikitouroku@gmail.com>
2024-09-06 22:27:55 +0900

    add change to game if fn

---
+++ game.go
@@ -0,0 +1,42 @@
+package scene
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func ToGame(scene Scene, layoutFn func(outsideWidth, outsideHeight int) (screenWidth, screenHeight int)) ebiten.Game {
+	return &game{
+		scene:    scene,
+		layoutFn: layoutFn,
+	}
+}
+
+type game struct {
+	inited   bool
+	scene    Scene
+	layoutFn func(outsideWidth, outsideHeight int) (screenWidth, screenHeight int)
+}
+
+func (g *game) Update() error {
+	if !g.inited {
+		g.scene.Init()
+	}
+	if g.scene.Ended() {
+		return ebiten.Termination
+	}
+	return g.Update()
+}
+
+func (g *game) Draw(screen *ebiten.Image) {
+	if !g.inited {
+		return
+	}
+	if g.scene.Ended() {
+		return
+	}
+	g.scene.Draw(screen)
+}
+
+func (g *game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return g.layoutFn(outsideWidth, outsideHeight)
+}
