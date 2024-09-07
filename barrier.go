eb8691c26a72c20b09ea4ed82ef2a8a01c398f7c
noppikinatta <noppikitouroku@gmail.com>
2024-09-07 13:47:52 +0900

    rename type

---
+++ barrier.go
@@ -0,0 +1,28 @@
+package scene
+
+import "github.com/hajimehoshi/ebiten/v2"
+
+type Barrier struct {
+	targetFn func() bool
+}
+
+func NewWait(targetFn func() bool) *Barrier {
+	return &Barrier{targetFn: targetFn}
+}
+
+func (b *Barrier) Init() {
+}
+
+func (b *Barrier) Update() error {
+	return nil
+}
+
+func (b *Barrier) Draw(screen *ebiten.Image) {
+}
+
+func (b *Barrier) Ended() bool {
+	return b.targetFn()
+}
+
+func (b *Barrier) Dispose() {
+}
