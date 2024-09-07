d8f631e204db905b3c84ff71fc6c3d6e34137c63
noppikinatta <noppikitouroku@gmail.com>
2024-09-07 10:45:21 +0900

    remove not needed interfaces

--- scene.go
+++ scene.go
@@ -5,25 +5,9 @@ import (
 )
 
 type Scene interface {
-	Initer
-	Updater
-	Drawer
-	Ended() bool
-	Disposer
-}
-
-type Initer interface {
 	Init()
-}
-
-type Updater interface {
 	Update() error
-}
-
-type Drawer interface {
 	Draw(screen *ebiten.Image)
-}
-
-type Disposer interface {
+	Ended() bool
 	Dispose()
 }
