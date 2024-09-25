281dcc52c2764105d9ef4fb460c0ab046066a3a5
noppikinatta <noppikitouroku@gmail.com>
2024-09-25 22:34:20 +0900

    🔧 more closer to actual use case

--- examples/chain/main.go
+++ examples/chain/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"image"
 	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 	"github.com/noppikinatta/scene"
 	"github.com/noppikinatta/scene/sceneutil"
 )
@@ -17,6 +19,8 @@ func main() {
 		return outsideWidth, outsideHeight
 	})
 
+	ebiten.SetWindowSize(600, 600)
+
 	err := ebiten.RunGame(g)
 	if err != nil {
 		log.Fatal(err)
@@ -25,178 +29,169 @@ func main() {
 
 func createScenes() scene.Scene {
 	const (
-		n1 = "red"
-		n2 = "green"
-		n3 = "blue"
-		n4 = "yellow"
-		n5 = "purple"
+		name1 = "red"
+		name2 = "green"
+		name3 = "blue"
+		name4 = "yellow"
+		name5 = "purple"
 	)
 
-	recorder := mouseButtonDownRecorder{}
+	nextScener := exampleNextScener{}
 
-	s1 := &exampleScene{
-		name:       n1,
-		color:      color.RGBA{R: 200, A: 255},
-		recorder:   &recorder,
-		transition: &mouseButtonTransition{Left: n2, Right: n3},
-	}
-	s2 := &exampleScene{
-		name:       n2,
-		color:      color.RGBA{G: 180, A: 255},
-		recorder:   &recorder,
-		transition: &mouseButtonTransition{Left: n4, Right: n1},
-	}
-	s3 := &exampleScene{
-		name:       n3,
-		color:      color.RGBA{B: 200, A: 255},
-		recorder:   &recorder,
-		transition: &mouseButtonTransition{Left: n5, Right: n4},
-	}
-	s4 := &exampleScene{
-		name:       n4,
-		color:      color.RGBA{R: 200, G: 180, A: 255},
-		recorder:   &recorder,
-		transition: &mouseButtonTransition{Left: n2, Right: n1},
-	}
-	s5 := &exampleScene{
-		name:       n5,
-		color:      color.RGBA{R: 200, B: 200, A: 255},
-		recorder:   &recorder,
-		transition: &mouseButtonTransition{Left: n3, Right: "EXIT GAME"},
-	}
+	scene1 := newExampleScene(name1, color.RGBA{R: 200, A: 255}, &nextScener, name2, name3)
+	scene2 := newExampleScene(name2, color.RGBA{G: 160, A: 255}, &nextScener, name4, name1)
+	scene3 := newExampleScene(name3, color.RGBA{B: 200, A: 255}, &nextScener, name5, name4)
+	scene4 := newExampleScene(name4, color.RGBA{R: 200, G: 160, A: 255}, &nextScener, name2, name1)
+	scene5 := newExampleScene(name5, color.RGBA{R: 200, B: 200, A: 255}, &nextScener, name3, "EXIT")
 
-	fs1 := sceneutil.WithSimpleFade(s1, 15, color.Black)
-	fs2 := sceneutil.WithSimpleFade(s2, 15, color.Black)
-	fs3 := sceneutil.WithSimpleFade(s3, 15, color.Black)
-	fs4 := sceneutil.WithSimpleFade(s4, 15, color.Black)
-	fs5 := sceneutil.WithSimpleFade(s5, 15, color.Black)
+	withFade := func(s scene.Scene) scene.Scene {
+		return sceneutil.WithSimpleFade(s, 15, color.Black)
+	}
 
 	scenes := map[string]scene.Scene{
-		n1: fs1,
-		n2: fs2,
-		n3: fs3,
-		n4: fs4,
-		n5: fs5,
+		scene1.Name: withFade(scene1),
+		scene2.Name: withFade(scene2),
+		scene3.Name: withFade(scene3),
+		scene4.Name: withFade(scene4),
+		scene5.Name: withFade(scene5),
 	}
 
-	transitions := map[scene.Scene]*mouseButtonTransition{
-		fs1: s1.transition,
-		fs2: s2.transition,
-		fs3: s3.transition,
-		fs4: s4.transition,
-		fs5: s5.transition,
-	}
+	nextScener.Scenes = scenes
 
-	ns := mouseButtonNextScener{
-		scenes:      scenes,
-		transitions: transitions,
-		recorder:    &recorder,
-	}
-
-	return scene.NewChain(fs1, &ns)
+	return scene.NewChain(scenes[scene1.Name], &nextScener)
 }
 
 type exampleScene struct {
-	name       string
-	waitFrames int
-	color      color.Color
-	ended      bool
-	transition *mouseButtonTransition
-	recorder   *mouseButtonDownRecorder
+	scene.Scene
+	Name    string
+	Buttons []*exampleButton
+	color   color.Color
+	ended   bool
+}
+
+func newExampleScene(name string, color color.Color, nextScener *exampleNextScener, nextSceneNames ...string) *exampleScene {
+	s := &exampleScene{
+		Name:  name,
+		color: color,
+	}
+
+	setNextScene := func(args any) {
+		sceneName := args.(string)
+		nextScener.SetNextSceneName(sceneName)
+	}
+
+	setEnded := func(any) {
+		s.SetEnded(true)
+	}
+
+	buttons := make([]*exampleButton, 0, len(nextSceneNames))
+	for _, n := range nextSceneNames {
+		buttons = append(buttons, &exampleButton{
+			NextSceneName: n,
+			EventHandlers: []func(any){
+				setNextScene,
+				setEnded,
+			},
+		})
+	}
+
+	s.Buttons = buttons
+	return s
 }
 
 func (s *exampleScene) Init() {
-	s.waitFrames = 15
-	s.recorder.Clear()
 	s.ended = false
 }
 
 func (s *exampleScene) Update() error {
-	if s.waitFrames > 0 {
-		s.waitFrames--
-		return nil
-	}
-
-	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
-		s.ended = true
-		s.recorder.Set(ebiten.MouseButtonLeft)
-	}
-	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
-		s.ended = true
-		s.recorder.Set(ebiten.MouseButtonRight)
+	for _, b := range s.Buttons {
+		b.Update()
 	}
 	return nil
 }
 
 func (s *exampleScene) Draw(screen *ebiten.Image) {
 	screen.Fill(s.color)
+	ebitenutil.DebugPrint(screen, s.Name)
 
-	txt := fmt.Sprintf(
-		"current: %s\nleft click: %s\nright click: %s",
-		s.name,
-		s.transition.Left,
-		s.transition.Right,
-	)
-
-	ebitenutil.DebugPrint(screen, txt)
+	for i, b := range s.Buttons {
+		if b.Bounds.Empty() {
+			b.Bounds = image.Rect(i*100, 30, i*100+80, 80)
+		}
+		b.Draw(screen)
+	}
 }
 
 func (s *exampleScene) Ended() bool {
 	return s.ended
 }
 
+func (s *exampleScene) SetEnded(value bool) {
+	s.ended = value
+}
+
 func (s *exampleScene) Dispose() {
+
 }
 
-type mouseButtonDownRecorder struct {
-	Pressed bool
-	Button  ebiten.MouseButton
+type exampleButton struct {
+	NextSceneName string
+	Bounds        image.Rectangle
+	EventHandlers []func(args any)
 }
 
-func (r *mouseButtonDownRecorder) Set(button ebiten.MouseButton) {
-	r.Pressed = true
-	r.Button = button
+func (b *exampleButton) Update() {
+	if !inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
+		return
+	}
+
+	if !b.contains(ebiten.CursorPosition()) {
+		return
+	}
+
+	for _, h := range b.EventHandlers {
+		h(b.NextSceneName)
+	}
 }
 
-func (r *mouseButtonDownRecorder) Clear() {
-	r.Pressed = false
-}
-
-func (r *mouseButtonDownRecorder) IsButtonDown(button ebiten.MouseButton) bool {
-	if !r.Pressed {
+func (b *exampleButton) contains(x, y int) bool {
+	if x < b.Bounds.Min.X {
 		return false
 	}
-	return button == r.Button
-}
-
-type mouseButtonTransition struct {
-	Left  string
-	Right string
-}
-
-type mouseButtonNextScener struct {
-	scenes      map[string]scene.Scene
-	transitions map[scene.Scene]*mouseButtonTransition
-	recorder    *mouseButtonDownRecorder
-}
-
-func (m *mouseButtonNextScener) NextScene(current scene.Scene) (scene.Scene, bool) {
-	t, ok := m.transitions[current]
-	if !ok {
-		return nil, false
+	if x > b.Bounds.Max.X {
+		return false
+	}
+	if y < b.Bounds.Min.Y {
+		return false
+	}
+	if y > b.Bounds.Max.Y {
+		return false
 	}
 
-	if m.recorder.IsButtonDown(ebiten.MouseButtonLeft) {
-		return m.scene(t.Left)
-	}
-	if m.recorder.IsButtonDown(ebiten.MouseButtonRight) {
-		return m.scene(t.Right)
-	}
-
-	return nil, false
+	return true
 }
 
-func (m *mouseButtonNextScener) scene(name string) (scene.Scene, bool) {
-	s, ok := m.scenes[name]
+func (b *exampleButton) Draw(screen *ebiten.Image) {
+	var x, y, w, h float32
+	x = float32(b.Bounds.Min.X)
+	y = float32(b.Bounds.Min.Y)
+	w = float32(b.Bounds.Dx())
+	h = float32(b.Bounds.Dy())
+
+	vector.DrawFilledRect(screen, x, y, w, h, color.RGBA{B: 128, A: 128}, false)
+	ebitenutil.DebugPrintAt(screen, b.NextSceneName, b.Bounds.Min.X+4, b.Bounds.Min.Y+4)
+}
+
+type exampleNextScener struct {
+	Scenes        map[string]scene.Scene
+	NextSceneName string
+}
+
+func (n *exampleNextScener) SetNextSceneName(name string) {
+	n.NextSceneName = name
+}
+
+func (n *exampleNextScener) NextScene(current scene.Scene) (scene.Scene, bool) {
+	s, ok := n.Scenes[n.NextSceneName]
 	return s, ok
 }
