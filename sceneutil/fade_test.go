4f2a7b1d0935c384c742b40e840abe99d8b78313
noppikinatta <noppikitouroku@gmail.com>
2024-10-03 20:29:34 +0900

    🧪 add fade test

--- sceneutil/fade_test.go
+++ sceneutil/fade_test.go
@@ -2,6 +2,7 @@ package sceneutil_test
 
 import (
 	"image/color"
+	"os"
 	"testing"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -47,15 +48,22 @@ func TestFade(t *testing.T) {
 }
 
 func TestWithSimpleFade(t *testing.T) {
-	ds := dummyScene{drawFn: func(screen *ebiten.Image) {
-		screen.Fill(color.Black)
-	}}
+	ds := dummyScene{
+		drawFn: func(screen *ebiten.Image) {
+			screen.Fill(color.Black)
+		},
+		endedFn: func() bool {
+			return true
+		},
+	}
 
 	s := sceneutil.WithSimpleFade(&ds, 4, color.RGBA{R: 20, G: 40, B: 80, A: 255})
 
-	img := ebiten.NewImage(10, 10)
+	img := ebiten.NewImage(3, 3)
 
 	s.Init()
+
+	colors := make([]color.Color, 0)
 
 	// i for avoid inf loop
 	for i := 0; i < 1000; i++ {
@@ -65,8 +73,39 @@ func TestWithSimpleFade(t *testing.T) {
 		}
 
 		s.Draw(img)
+		colors = append(colors, img.At(1, 1)) // get center pixel
 		if s.Ended() {
 			break
+		}
+	}
+
+	expectedColors := []color.Color{
+		color.RGBA{R: 15, G: 30, B: 60, A: 255}, // 0.25
+		color.RGBA{R: 10, G: 20, B: 40, A: 255}, // 0.50
+		color.RGBA{R: 5, G: 10, B: 20, A: 255},  // 0.75
+		color.Black,                             // 1.00
+		color.Black,
+		color.RGBA{R: 5, G: 10, B: 20, A: 255},  // 0.25
+		color.RGBA{R: 10, G: 20, B: 40, A: 255}, // 0.50
+		color.RGBA{R: 15, G: 30, B: 60, A: 255}, // 0.75
+		color.RGBA{R: 20, G: 40, B: 80, A: 255}, // 1.00
+	}
+
+	if len(expectedColors) != len(colors) {
+		t.Fatalf("expected color len %d but got %d", len(expectedColors), len(colors))
+	}
+
+	rgba := func(c color.Color) color.Color {
+		r, g, b, a := c.RGBA()
+		return color.RGBA{R: uint8(r), G: uint8(g), B: uint8(b), A: uint8(a)}
+	}
+
+	for i := range colors {
+		ec := rgba(expectedColors[i])
+		ac := rgba(colors[i])
+
+		if ec != ac {
+			t.Errorf("%d:expected color %v but got %v", i, ec, ac)
 		}
 	}
 
@@ -122,4 +161,40 @@ func (s *dummyScene) Dispose() {
 		return
 	}
 	s.disposeFn()
+}
+
+// from: https://github.com/hajimehoshi/ebiten/blob/main/internal/testing/testing.go
+type game struct {
+	m    *testing.M
+	code int
+}
+
+func (g *game) Update() error {
+	g.code = g.m.Run()
+	return ebiten.Termination
+}
+
+func (*game) Draw(*ebiten.Image) {
+}
+
+func (*game) Layout(int, int) (int, int) {
+	return 320, 240
+}
+
+func MainWithRunLoop(m *testing.M) {
+	// Run an Ebiten process so that (*Image).At is available.
+	g := &game{
+		m:    m,
+		code: 1,
+	}
+	if err := ebiten.RunGame(g); err != nil {
+		panic(err)
+	}
+	if g.code != 0 {
+		os.Exit(g.code)
+	}
+}
+
+func TestMain(m *testing.M) {
+	MainWithRunLoop(m)
 }
