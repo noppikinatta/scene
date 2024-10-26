495b51e5f886dcb93490150d4f64a9893a0be2b5
noppikinatta <noppikitouroku@gmail.com>
2024-10-26 10:39:38 +0900

    ✏️ add comments

--- sequence.go
+++ sequence.go
@@ -6,16 +6,19 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+// Sequence provides the ability to run multiple ebiten.Games in sequence.
 type Sequence struct {
 	current           ebiten.Game
 	transitionUpdater *transitionUpdater
 	inited            bool
 }
 
+// NewSequence creates a new Sequence instance.
 func NewSequence(first ebiten.Game) *Sequence {
 	return &Sequence{current: first}
 }
 
+// Update is ebiten.Game.Update implementation.
 func (s *Sequence) Update() error {
 	if s.inTransition() {
 		if err := s.transitionUpdater.Update(); err != nil {
@@ -36,6 +39,7 @@ func (s *Sequence) Update() error {
 	return err
 }
 
+// Draw is ebiten.Game.Draw implementation.
 func (s *Sequence) Draw(screen *ebiten.Image) {
 	s.current.Draw(screen)
 	if s.inTransition() {
@@ -43,14 +47,17 @@ func (s *Sequence) Draw(screen *ebiten.Image) {
 	}
 }
 
+// Layout is ebiten.Game.Layout implementation.
 func (s *Sequence) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return s.current.Layout(outsideWidth, outsideHeight)
 }
 
+// Switch switches the ebiten.Game to run in Sequence.
 func (s *Sequence) Switch(next ebiten.Game) bool {
 	return s.SwitchWithTransition(next, NopTransition)
 }
 
+// SwitchWithTransition switches the ebiten.Game to run in Sequence with the Transition.
 func (s *Sequence) SwitchWithTransition(next ebiten.Game, transition Transition) bool {
 	if s.inTransition() {
 		return false
@@ -62,21 +69,25 @@ func (s *Sequence) SwitchWithTransition(next ebiten.Game, transition Transition)
 	return true
 }
 
+// inTransition returns true if the Transition is being processed.
 func (s *Sequence) inTransition() bool {
 	return s.transitionUpdater != nil
 }
 
+// switchScenes switches scenes.
 func (s *Sequence) switchScenes(next ebiten.Game) {
 	callIfImpl(s.current, func(o OnEnder) { o.OnEnd() })
 	s.current = next
 	callIfImpl(s.current, func(o OnStarter) { o.OnStart() })
 }
 
+// endTransition is called when the Transition completed.
 func (s *Sequence) endTransition() {
 	s.transitionUpdater = nil
 	callIfImpl(s.current, func(o OnArrivaler) { o.OnArrival() })
 }
 
+// drawFinalScreenFunc is FinalScreenDrawerConvertible implementation.
 func (p *Sequence) drawFinalScreenFunc() func(screen ebiten.FinalScreen, offScreen *ebiten.Image, geoM ebiten.GeoM) {
 	return func(screen ebiten.FinalScreen, offScreen *ebiten.Image, geoM ebiten.GeoM) {
 		if f, ok := p.current.(ebiten.FinalScreenDrawer); ok {
@@ -87,6 +98,7 @@ func (p *Sequence) drawFinalScreenFunc() func(screen ebiten.FinalScreen, offScre
 	}
 }
 
+// layoutFFunc is LayoutFConvertible implementation.
 func (p *Sequence) layoutFFunc() func(outsideWidth, outsideHeight float64) (screenWidth, screenHeight float64) {
 	return func(outsideWidth, outsideHeight float64) (screenWidth float64, screenHeight float64) {
 		if l, ok := p.current.(ebiten.LayoutFer); ok {
