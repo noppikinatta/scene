7525872d94f36d1247eaf8f380184b13315bf7b1
noppikinatta <noppikitouroku@gmail.com>
2024-10-26 19:01:57 +0900

    ✏️ add comments

--- scene.go
+++ scene.go
@@ -2,18 +2,24 @@ package scene
 
 import "github.com/hajimehoshi/ebiten/v2"
 
+// OnStarter is an interface that executes processing at the start of a scene.
 type OnStarter interface {
+	// OnStart　is called immediately after the start of the scene.
 	OnStart()
 }
 
+// OnEnder is an interface that executes processing at the end of a scene.
 type OnEnder interface {
+	// OnEnd is called just before the end of the scene.
 	OnEnd()
 }
 
+// OnArrivaler is an interface that executes processing when a scene begins and the beginning transition is completed.
 type OnArrivaler interface {
 	OnArrival()
 }
 
+// OnDeparturer is an interface that executes processing when a scene ends and the ending transition begins.
 type OnDeparturer interface {
 	OnDeparture()
 }
--- transition.go
+++ transition.go
@@ -4,14 +4,25 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+// Transition is an interface for drawing during scene transitions.
 type Transition interface {
+	// Reset is called at the start of a scene transition.
+	// If the same instance is to be used multiple times, Reset should be used to initialize the Transition state.
 	Reset()
+	// Update is called in ebiten.Game.Update to update the state of Transition.
 	Update() error
+	// Draw draws during scene transitions.
+	// The argument screen reflects the result of drawing by ebiten.Game.Draw.
 	Draw(screen *ebiten.Image)
+	// Completed returns true when the scene transition is complete.
 	Completed() bool
+	// CanSwitchScenes returns true when ebiten.Game, which represents a scene, can be switched.
+	// Even if true is returned at multiple frames during a single scene transition, the scene is switched only once.
+	// If CanSwitchScenes does not return true, the scene switch will occur when Completed returns true.
 	CanSwitchScenes() bool
 }
 
+// NopTransition is a Transition that does not draw anything.
 var NopTransition Transition = nopTransition{}
 
 type nopTransition struct{}
@@ -22,37 +33,47 @@ func (t nopTransition) Draw(*ebiten.Image)    {}
 func (t nopTransition) Completed() bool       { return true }
 func (t nopTransition) CanSwitchScenes() bool { return true }
 
+// LinearTransition is a Transition that transitions linearly for a specified number of frames.
 type LinearTransition struct {
 	currentFrame int
 	maxFrames    int
 	drawer       LinearTransitionDrawer
 }
 
+// NewLinearTransition returns a new LinearTransition.
 func NewLinearTransition(maxFrames int, drawer LinearTransitionDrawer) *LinearTransition {
 	return &LinearTransition{maxFrames: maxFrames, drawer: drawer}
 }
 
+// LinearTransitionDrawer is an interface that draws as the LinearTransition progresses.
 type LinearTransitionDrawer interface {
+	// Draw draws as the LinearTransition progresses.
 	Draw(screen *ebiten.Image, progress LinearTransitionProgress)
 }
 
+// LinearTransitionProgress represents the progress of LinearTransition.
 type LinearTransitionProgress struct {
-	CurrentFrame int
-	MaxFrames    int
+	CurrentFrame int // CurrentFrame is the current frame.
+	MaxFrames    int // MaxFrames is the maximum number of frames.
 }
 
+// Rate returns the progress rate of LinearTransition in the range of 0.0~1.0.
 func (p LinearTransitionProgress) Rate() float64 {
 	return float64(p.CurrentFrame) / float64(p.MaxFrames)
 }
 
+// Halfway returns true when progress is exactly halfway.
 func (p LinearTransitionProgress) Halfway() bool {
 	return p.CurrentFrame == (p.MaxFrames/2 + 1)
 }
 
+// Reset is called at the start of a scene transition.
+// If the same instance is to be used multiple times, Reset should be used to initialize the Transition state.
 func (t *LinearTransition) Reset() {
 	t.currentFrame = 0
 }
 
+// Update is called in ebiten.Game.Update to update the state of Transition.
 func (t *LinearTransition) Update() error {
 	if t.Completed() {
 		return nil
@@ -61,11 +82,14 @@ func (t *LinearTransition) Update() error {
 	return nil
 }
 
+// Draw draws during scene transitions.
+// The argument screen reflects the result of drawing by ebiten.Game.Draw.
 func (t *LinearTransition) Draw(screen *ebiten.Image) {
 	p := t.Progress()
 	t.drawer.Draw(screen, p)
 }
 
+// Progress returns the progress of it.
 func (t *LinearTransition) Progress() LinearTransitionProgress {
 	return LinearTransitionProgress{
 		CurrentFrame: t.currentFrame,
@@ -73,10 +97,14 @@ func (t *LinearTransition) Progress() LinearTransitionProgress {
 	}
 }
 
+// Completed returns true when the scene transition is complete.
 func (t *LinearTransition) Completed() bool {
 	return t.currentFrame > t.maxFrames
 }
 
+// CanSwitchScenes returns true when ebiten.Game, which represents a scene, can be switched.
+// Even if true is returned at multiple frames during a single scene transition, the scene is switched only once.
+// If CanSwitchScenes does not return true, the scene switch will occur when Completed returns true.
 func (t *LinearTransition) CanSwitchScenes() bool {
 	return t.Progress().Halfway()
 }
